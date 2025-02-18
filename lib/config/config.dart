244fa8daf909414880423eb4baf55eaa6aaa6c3e
Aailmz <mirzazubaridjunaid@gmail.com>
2025-02-18 12:42:06 +0700

    feat: add comment for tutor

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -1,5 +1,5 @@
 class ApiConfig {
-  static const String baseUrl = 'https://1ffd-2404-c0-2020-00-712-d2e0.ngrok-free.app/api';
+  static const String baseUrl = 'https://1ffd-2404-c0-2020-00-712-d2e0.ngrok-free.app/api';// Your Ngrok/whatever URL
   static String get loginUrl => '$baseUrl/auth/login';
   static String get registerUrl => '$baseUrl/auth/register';
   static String get productsUrl => '$baseUrl/produk';
